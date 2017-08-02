53d2303467111fb81fa52a83c7880fd3d6573452
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-02 21:27:03 +0700

    xong

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const parser = require('body-parser').urlencoded({ extended: false });
 const upload = require('./uploadConfig');
 
 const multerParser = upload.single('avatar');
+const fileArrayParser = upload.array('hinhsanpham');
+
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -16,9 +18,30 @@ app.post('/signup', multerParser, (req, res) => {
     res.send('Da nhan');
 });
 
-app.post('/xuly', parser, (req, res) => {
-    console.log(req.body);
+app.post('/sanpham', fileArrayParser, (req, res) => {
+    console.log(req.body, req.files);
     res.send('Da nhan');
 });
 
+app.post('/xuly', parser, (req, res) => {
+    console.log(req.body);
+    res.status(200).send('Da nhan');
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.send('CO LOI');
+});
+
 app.listen(3000, () => console.log('Server started!'));
+
+// 200
+// 201 
+// 404 
+
+class MyError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.code = code;
+    }
+}
